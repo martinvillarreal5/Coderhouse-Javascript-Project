2076555941672600c32f4e7356f8604ba0edec09
Martin Villarreal <martinvictorvillarreal@gmail.com>
2022-02-22 20:27:25 -0300

    test

--- js/main.js
+++ js/main.js
@@ -256,7 +256,7 @@ const products = [];
 
 $(document).ready(function () {
   // debe ejecutarse desde el index para que el if se cumpla
-  if (window.location.pathname == "/") {
+  if (window.location.pathname == "/index") {
     $.getJSON("json/products.json", function (data) {
       data.forEach(function (jsonitem) {
         products.push(
